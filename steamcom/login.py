671acd290dd6e80f87cb4fe0581bedcfb37e369d
Linar Sharifullin <48877848+LinarSharifullin@users.noreply.github.com>
2022-05-30 11:36:37 +0700

    From LoginExecutor created _fetch_rsa_params method

--- steamcom/login.py
+++ steamcom/login.py
@@ -14,3 +14,12 @@ class LoginExecutor:
         self.password = password
         self.shared_secret = shared_secret
         self.session = session
+
+    def _fetch_rsa_params(self) -> dict:
+        key_response = self.session.post(SteamUrl.COMMUNITY_URL
+            + '/login/getrsakey/', data={'username': self.username}).json()
+        rsa_mod = int(key_response['publickey_mod'], 16)
+        rsa_exp = int(key_response['publickey_exp'], 16)
+        rsa_timestamp = key_response['timestamp']
+        return {'rsa_key': rsa.PublicKey(rsa_mod, rsa_exp),
+            'rsa_timestamp': rsa_timestamp}
