fd7f0242c57073f991084d5da6c9f16be61461e8
Linar Sharifullin <48877848+LinarSharifullin@users.noreply.github.com>
2022-06-01 16:33:39 +0700

    Added get_confirmations method

--- steamcom/confirmations.py
+++ steamcom/confirmations.py
@@ -1,8 +1,11 @@
 import requests
 import time
+from typing import List
+
+from bs4 import BeautifulSoup
 
 from steamcom.guard import generate_confirmation_key, generate_device_id
-from steamcom.models import Tag
+from steamcom.models import Tag, Confirmation
 
 
 class ConfirmationExecutor:
@@ -13,6 +16,38 @@ class ConfirmationExecutor:
         self._my_steam_id = my_steam_id
         self._identity_secret = identity_secret
         self._session = session
+
+    def get_confirmations(self) -> List[Confirmation]:
+        confirmations = []
+        confirmations_page = self._fetch_confirmations_page()
+        soup = BeautifulSoup(confirmations_page.text, 'html.parser')
+        if soup.select('#mobileconf_empty'):
+            return confirmations
+        container = soup.find(id="mobileconf_list")
+        entries = container.find_all(class_="mobileconf_list_entry")
+        for entry in entries:
+            description = entry.select(
+                ".mobileconf_list_entry_description > div")
+            img = entry.select(".mobileconf_list_entry_icon img")[0]
+            confirmations.append(Confirmation(
+                conf_id = entry['data-confid'],
+                conf_type = entry['data-type'],
+                data_accept = entry['data-accept'],
+                creator = entry['data-creator'],
+                key = entry['data-key'],
+                title = description[0].string,
+                receiving = description[1].string,
+                time = description[2].string,
+                icon = img['src'] if len(img['src']) > 0 else "",
+            ))
+        return confirmations
+
+    def _fetch_confirmations_page(self) -> requests.Response:
+        tag = Tag.CONF.value
+        params = self._create_confirmation_params(tag)
+        response = self._session.get(self.CONF_URL + '/conf', params=params)
+        return response
+
 
     def _create_confirmation_params(self, tag_string: str) -> dict:
         timestamp = int(time.time())
@@ -28,9 +63,3 @@ class ConfirmationExecutor:
             'tag': tag_string
         }
         return params
-
-    def _fetch_confirmations_page(self) -> requests.Response:
-        tag = Tag.CONF.value
-        params = self._create_confirmation_params(tag)
-        response = self._session.get(self.CONF_URL + '/conf', params=params)
-        return response
