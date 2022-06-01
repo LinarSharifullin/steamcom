63056347cee69285b0c829ab76a1d5bde68af65c
Linar Sharifullin <48877848+LinarSharifullin@users.noreply.github.com>
2022-06-01 09:25:07 +0700

    Add model Confirmation

--- steamcom/models.py
+++ steamcom/models.py
@@ -2,3 +2,16 @@ class SteamUrl:
     API_URL = 'https://api.steampowered.com'
     COMMUNITY_URL = 'https://steamcommunity.com'
     STORE_URL = 'https://store.steampowered.com'
+
+
+class Confirmation:
+    def __init__(self, _id, conf_type, creator, key, title, receiving, 
+            time, icon):
+        self.id = _id.split('conf')[1]
+        self.conf_type = conf_type
+        self.creator = creator
+        self.key = key
+        self.title = title
+        self.receiving = receiving
+        self.time = time
+        self.icon = icon
