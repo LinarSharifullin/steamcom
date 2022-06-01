6beff3db756d468cbcf97bfa61498d8e77bbad98
Linar Sharifullin <48877848+LinarSharifullin@users.noreply.github.com>
2022-06-01 11:58:29 +0700

    Created ConfirmationExecutor

---
+++ steamcom/confirmations.py
@@ -0,0 +1,28 @@
+import requests
+import time
+
+from guard import generate_confirmation_key, generate_device_id
+
+
+class ConfirmationExecutor:
+    CONF_URL = "https://steamcommunity.com/mobileconf"
+
+    def __init__(self, identity_secret: str, my_steam_id: str, session: requests.Session) -> None:
+        self._my_steam_id = my_steam_id
+        self._identity_secret = identity_secret
+        self._session = session
+
+    def _create_confirmation_params(self, tag_string: str) -> dict:
+        timestamp = int(time.time())
+        confirmation_key = generate_confirmation_key(self._identity_secret, 
+            tag_string, timestamp)
+        android_id = generate_device_id(self._my_steam_id)
+        params = {
+            'p': android_id,
+            'a': self._my_steam_id,
+            'k': confirmation_key,
+            't': timestamp,
+            'm': 'android',
+            'tag': tag_string
+        }
+        return params
