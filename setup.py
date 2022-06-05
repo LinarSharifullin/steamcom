4e7afbf68fc3d6aa9ed91566ff0cfe32de9706af
Linar Sharifullin <48877848+LinarSharifullin@users.noreply.github.com>
2022-06-05 20:39:09 +0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='steamcom',
+    packages=['steamcom'],
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'rsa',
+        'pycryptodomex'
+    ],
+)
