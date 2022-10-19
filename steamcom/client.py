01e2df05a3effbd75d0dacd874c58a5b089ed760
Linar Sharifullin <48877848+LinarSharifullin@users.noreply.github.com>
2022-10-19 12:52:38 +0600

    feat: add param session in SteamClient

--- steamcom/client.py
+++ steamcom/client.py
@@ -17,12 +17,13 @@ from steamcom.market import SteamMarket
 class SteamClient:
 
     def __init__(self, username: str = '', password: str = '',
-                 shared_secret: str = '', identity_secret: str = '') -> None:
+                 shared_secret: str = '', identity_secret: str = '',
+                 session: requests.Session = requests.Session()) -> None:
         self.username = username
         self.password = password
         self.shared_secret = shared_secret
         self.identity_secret = identity_secret
-        self.session = requests.Session()
+        self.session = session
         self.steam_id = ''  # will be added after login
         self.currency_id = None  # will be added after login
         self.was_login_executed = False
