0f5830466a302291e6b64c68792736595cd2f2b4
Linar Sharifullin <48877848+LinarSharifullin@users.noreply.github.com>
2025-12-08 12:02:51 +0700

    refactor: classFeeCounter

--- steamcom/fee_counter.py
+++ steamcom/fee_counter.py
@@ -1,66 +1,70 @@
 import math
 
 
-WALLET_FEE_BASE = 0
-WALLET_FEE_PERCENT = 0.05
-WALLET_FEE_MINIMUM = 1
+
+class FeeCounter:
+    def __init__(self, wallet_fee_base: int = 0, wallet_fee_percent: float = 0.05,
+                 wallet_fee_minimum: int = 1):
+        self.wallet_fee_base = wallet_fee_base
+        self.wallet_fee_percent = wallet_fee_percent
+        self.wallet_fee_minimum = wallet_fee_minimum
+
+    def get_int_price(self, strAmount):
+        if not strAmount:
+            return 0
+        elif strAmount < 0.03:
+            strAmount = 0.03
+        flAmount = float(strAmount) * 100
+        nAmount = math.floor(0 if not type(flAmount) else flAmount + 0.000001)
+        nAmount = max(nAmount, 0)
+        return nAmount
 
 
-def get_int_price(strAmount):
-    if not strAmount:
-        return 0
-    elif strAmount < 0.03:
-        strAmount = 0.03
-    flAmount = float(strAmount) * 100
-    nAmount = math.floor(0 if not type(flAmount) else flAmount + 0.000001)
-    nAmount = max(nAmount, 0)
-    return nAmount
+    def calculate_fee_amount(self, amount, publisherFee):
+        publisherFee = 0.1
+        iterations = 0
+        received = int((amount - self.wallet_fee_base) / (
+            self.wallet_fee_percent + float(publisherFee) + 1))
+        bEverUndershot = False
+        fees = self.sended(received, publisherFee)
 
-
-def calculate_fee_amount(amount, publisherFee):
-    publisherFee = 0.1
-    iterations = 0
-    received = int((amount - int(WALLET_FEE_BASE)) / (
-        float(WALLET_FEE_PERCENT) + float(publisherFee) + 1))
-    bEverUndershot = False
-    fees = sended(received, publisherFee)
-
-    while (fees['amount'] != amount and iterations < 10):
-        if fees['amount'] > amount:
-            if bEverUndershot:
-                fees = sended(received-1, publisherFee)
-                fees['steam_fee'] += amount - fees['amount']
-                fees['amount'] = amount
-                break
+        while (fees['amount'] != amount and iterations < 10):
+            if fees['amount'] > amount:
+                if bEverUndershot:
+                    fees = self.sended(received-1, publisherFee)
+                    fees['steam_fee'] += amount - fees['amount']
+                    fees['amount'] = amount
+                    break
+                else:
+                    received -= 1
             else:
-                received -= 1
-        else:
-            bEverUndershot = True
-            received += 1
+                bEverUndershot = True
+                received += 1
 
-        fees = sended(received, publisherFee)
-        iterations += 1
+            fees = self.sended(received, publisherFee)
+            iterations += 1
 
-    return fees
+        return fees
 
 
-def sended(receivedAmount, publisherFee):
-    nSteamFee = int(math.floor(max(receivedAmount * float(WALLET_FEE_PERCENT),
-                    WALLET_FEE_MINIMUM) + int(WALLET_FEE_BASE)))
-    nPublisherFee = int(math.floor(max(
-        receivedAmount * publisherFee, 1) if publisherFee > 0 else 0))
-    nAmountToSend = receivedAmount + nSteamFee + nPublisherFee
-    return {
-        'steam_fee': nSteamFee,
-        'publisher_fee': nPublisherFee,
-        'fees': nSteamFee + nPublisherFee,
-        'buyer_pay': int(nAmountToSend),
-        'amount': int(nAmountToSend),
-        'seller_receive': receivedAmount
-    }
+    def sended(self, receivedAmount, publisherFee):
+        nSteamFee = int(math.floor(max(receivedAmount * self.wallet_fee_percent,
+                        self.wallet_fee_minimum) + self.wallet_fee_base))
+        nPublisherFee = int(math.floor(max(
+            receivedAmount * publisherFee, 1) if publisherFee > 0 else 0))
+        nAmountToSend = receivedAmount + nSteamFee + nPublisherFee
+        return {
+            'steam_fee': nSteamFee,
+            'publisher_fee': nPublisherFee,
+            'fees': nSteamFee + nPublisherFee,
+            'buyer_pay': int(nAmountToSend),
+            'amount': int(nAmountToSend),
+            'seller_receive': receivedAmount
+        }
 
 
-def count(price: float) -> dict:
-    priceInt = get_int_price(price)
-    fees = calculate_fee_amount(priceInt, 0.1)
-    return fees
+    def count(self, price: float) -> dict:
+        priceInt = self.get_int_price(price)
+        fees = self.calculate_fee_amount(priceInt, 0.1)
+        return fees
+
