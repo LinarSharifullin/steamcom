4909c06027c8022bd9dded50d8ddd28b9b5a0afa
Linar Sharifullin <48877848+LinarSharifullin@users.noreply.github.com>
2026-01-07 05:50:30 +0700

    test: refactor fee_counter tests

--- tests/test_fee_counter.py
+++ tests/test_fee_counter.py
@@ -1,60 +1,39 @@
 from steamcom.fee_counter import FeeCounter
 
+COMMISSION_TESTS = {
+    "RUB": {
+        "market_minimum": 79,
+        "currency_increment": 1,
+        "cases": [
+            (-1.5, 79, 237),
+            (0, 79, 237),
+            (0.79, 79, 237),
+            (2.37, 79, 237),
+            (2.38, 80, 238),
+            (9.55, 797, 955),
+            (9.56, 798, 956),
+            (9.58, 799, 957),
+            (9.68, 809, 968),
+            (9.79, 819, 979),
+            (9.80, 819, 979),
+            (9.81, 820, 981),
+            (10, 838, 1000),
+            (1150.09, 100009, 115009),
+            (1150.10, 100009, 115009),
+            (1150.11, 100010, 115011),
+            (100000.00, 8695653, 10000000),
+        ],
+    }
+}
 
-def test_rub():
-    fee_counter = FeeCounter(market_minimum=77)
-    assert fee_counter.calculate_seller_price(-1000) == 77
-    assert fee_counter.calculate_seller_price(-5.5) == 77
-    assert fee_counter.calculate_seller_price(-1) == 77
-    assert fee_counter.calculate_seller_price(0) == 77
-    assert fee_counter.calculate_seller_price(0.01) == 77
-    assert fee_counter.calculate_seller_price(1) == 77
-    assert fee_counter.calculate_seller_price(1.7) == 77
-    assert fee_counter.calculate_seller_price(2.31) == 77
-    assert fee_counter.calculate_seller_price(2.33) == 79
-    assert fee_counter.calculate_seller_price(3) == 146
-    assert fee_counter.calculate_seller_price(5.4) == 386
-    assert fee_counter.calculate_seller_price(10) == 845
-    assert fee_counter.calculate_seller_price(11.5) == 995
-    assert fee_counter.calculate_seller_price(12) == 1042
-    assert fee_counter.calculate_seller_price(120) == 10436
-    assert fee_counter.calculate_seller_price(1000) == 86958
-    assert fee_counter.calculate_seller_price(50000.99) == 4347913
 
-def test_kzt():
-    fee_counter = FeeCounter(market_minimum=500, currency_increment=100)
-    assert fee_counter.calculate_seller_price(-1000) == 500
-    assert fee_counter.calculate_seller_price(-5.5) == 500
-    assert fee_counter.calculate_seller_price(-1) == 500
-    assert fee_counter.calculate_seller_price(0) == 500
-    assert fee_counter.calculate_seller_price(0.01) == 500
-    assert fee_counter.calculate_seller_price(1) == 500
-    assert fee_counter.calculate_seller_price(10) == 500
-    assert fee_counter.calculate_seller_price(15) == 500
-    assert fee_counter.calculate_seller_price(16) == 600
-    assert fee_counter.calculate_seller_price(25) == 1500
-    assert fee_counter.calculate_seller_price(25.4) == 1500
-    assert fee_counter.calculate_seller_price(125) == 10900
-    assert fee_counter.calculate_seller_price(200.5) == 17400
-    assert fee_counter.calculate_seller_price(999) == 86900
-    assert fee_counter.calculate_seller_price(10000) == 869500
-    assert fee_counter.calculate_seller_price(100000) == 8695600
-    assert fee_counter.calculate_seller_price(300000) == 26086900
-
-def test_brl():
-    fee_counter = FeeCounter(market_minimum=5)
-    assert fee_counter.calculate_seller_price(-100) == 5
-    assert fee_counter.calculate_seller_price(-1.5) == 5
-    assert fee_counter.calculate_seller_price(0) == 5
-    assert fee_counter.calculate_seller_price(0.05) == 5
-    assert fee_counter.calculate_seller_price(0.15) == 5
-    assert fee_counter.calculate_seller_price(0.16) == 6
-    assert fee_counter.calculate_seller_price(0.18) == 8
-    assert fee_counter.calculate_seller_price(0.25) == 15
-    assert fee_counter.calculate_seller_price(0.99) == 86
-    assert fee_counter.calculate_seller_price(1.5) == 131
-    assert fee_counter.calculate_seller_price(2) == 175
-    assert fee_counter.calculate_seller_price(5.5) == 479
-    assert fee_counter.calculate_seller_price(100) == 8697
-    assert fee_counter.calculate_seller_price(10000) == 869566
-    assert fee_counter.calculate_seller_price(100000.5) == 8695697
+def test_calculate_seller_price():
+    for currency_data in COMMISSION_TESTS.values():
+        fc = FeeCounter(
+            market_minimum=currency_data["market_minimum"],
+            currency_increment=currency_data["currency_increment"],
+        )
+        for price, seller_receive, buyer_pay in currency_data["cases"]:
+            fee_price = fc.calculate_seller_price(price)
+            assert fee_price.seller_receive == seller_receive
+            assert fee_price.buyer_pay == buyer_pay
